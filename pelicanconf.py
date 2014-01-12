a42c7ffbd11701d7c2949aff2b13128ee1f2e6d3
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-01-12 15:31:56 +0000

    removed pelican-youtube

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,7 +15,7 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-PLUGINS = ['pelican_youtube', ]
+#PLUGINS = ['pelican_youtube', ]
 
 STATIC_PATHS = ['media', 'images', ]
 
